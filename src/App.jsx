26b64131590dc86daca8f05c4747a2858844d9f9
Sean Snelder <spsnelde@usc.edu>
2023-11-15 22:30:41 -0800

    first update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import JoinPage from "./join-page/JoinPage";
 import LandingPage from "./landing-page/LandingPage";
 import FullCompPage from "./competition-page/FullCompPage";
+import WaitingPage from "./waiting-page/WaitingPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/join" element={<JoinPage />} />
       <Route path="/competition" element={<FullCompPage />} />
+      <Route path="/waiting" element={<WaitingPage />} />
     </Routes>
   );
 }
