bed6984374b5018732b0c2f25fb22270307a2d9e
Yingxin Wu <wuyingxi@usc.edu>
2023-11-15 20:14:18 -0800

    added sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom";
 import JoinPage from "./join-page/JoinPage";
 import LandingPage from "./landing-page/LandingPage";
 import FullCompPage from "./competition-page/FullCompPage";
+import SignUp from "./sign-up-page/SignUp";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/join" element={<JoinPage />} />
       <Route path="/competition" element={<FullCompPage />} />
+      <Route path="/sign-up" element={<SignUp />}/>
     </Routes>
   );
 }
