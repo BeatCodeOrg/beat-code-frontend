33257d12a5312d8ec3612cbbb08eeb1868472471
VishnuVelayuthan <vishnuvelayuthan@gmail.com>
2023-11-27 18:35:41 -0800

    Finished styling the question type portion, now working on the difficulty portion

---
+++ src/waiting-page/QuestionTypeForm/DifficultyButton.jsx
@@ -0,0 +1,11 @@
+import "./DifficultyButton.css";
+
+function DifficultyButton() {
+  return (
+    <div>
+      <div className="ellipse"></div>
+    </div>
+  );
+}
+
+export default DifficultyButton;
--- src/waiting-page/QuestionTypeForm/QuestionTypeForm.jsx
+++ src/waiting-page/QuestionTypeForm/QuestionTypeForm.jsx
@@ -1,6 +1,7 @@
 import FilterChoice from "./FilterChoice";
+import DifficultyButton from "./DifficultyButton";
 
-function QuestionFilterForm() {
+function QuestionTypeForm() {
   const questionCategories = [
     "Algorithms",
     "Hash Table",
@@ -15,16 +16,23 @@ function QuestionFilterForm() {
 
   return (
     <div>
-      <form>
-        <h1>QUESTION TYPE</h1>
+      <form className="flex flex-col items-center">
+        <h1 className="font-semibold text-3xl my-2 tracking-wider">
+          QUESTION TYPE
+        </h1>
         <div className="flex flex-wrap " style={{ maxWidth: "15rem" }}>
           {questionCategories.map((category, ind) => (
             <FilterChoice key={ind} filterName={category} />
           ))}
         </div>
+        <h1 className="font-semibold text-3xl my-2 tracking-wider">
+          DIFFICULTY
+        </h1>
+        <img src="src/assets/waiting-page/Line.png" alt="line" />
+        <DifficultyButton />
       </form>
     </div>
   );
 }
 
-export default QuestionFilterForm;
+export default QuestionTypeForm;
