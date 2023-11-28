1cb9edd6260f3a2129f43e6bc75022637be1a5a0
VishnuVelayuthan <vishnuvelayuthan@gmail.com>
2023-11-27 20:32:49 -0800

    Got the form's styling mostly done, now working on collecting the form data?

--- src/waiting-page/QuestionTypeForm/DifficultyButton.jsx
+++ src/waiting-page/QuestionTypeForm/DifficultyButton.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+
 import "./DifficultyButton.css";
 
-function DifficultyButton() {
+function DifficultyButton({ label, updateQData }) {
+  const [selected, setSelected] = useState(false);
+
+  const handleClick = () => {
+    if (!selected) {
+      updateQData((prev) => ({
+        ...prev,
+        difficulty: [...prev.difficulty, label],
+      }));
+    } else {
+      updateQData((prev) => ({
+        ...prev,
+        difficulty: prev.difficulty.filter((d) => d !== label),
+      }));
+    }
+    setSelected(!selected);
+  };
   return (
-    <div>
-      <div className="ellipse"></div>
+    <div className="flex flex-col items-center" onClick={handleClick}>
+      <div className={`ellipse ${selected ? "filled-ellipse" : ""}`} />
+      <p className="text-sm">{label}</p>
     </div>
   );
 }
--- src/waiting-page/QuestionTypeForm/QuestionTypeForm.jsx
+++ src/waiting-page/QuestionTypeForm/QuestionTypeForm.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
+
 import FilterChoice from "./FilterChoice";
 import DifficultyButton from "./DifficultyButton";
 
+const questionCategories = [
+  "Algorithms",
+  "Hash Table",
+  "Dynamic Programming",
+  "Sorting",
+  "Array",
+  "Binary Search",
+  "Graph",
+  "Greedy",
+  "Priority Queue",
+];
+
 function QuestionTypeForm() {
-  const questionCategories = [
-    "Algorithms",
-    "Hash Table",
-    "Dynamic Programming",
-    "Sorting",
-    "Array",
-    "Binary Search",
-    "Graph",
-    "Greedy",
-    "Priority Queue",
-  ];
+  const [qFormData, setQFormData] = useState({
+    questionType: [],
+    difficulty: [],
+  });
 
   return (
     <div>
@@ -22,14 +29,28 @@ function QuestionTypeForm() {
         </h1>
         <div className="flex flex-wrap " style={{ maxWidth: "15rem" }}>
           {questionCategories.map((category, ind) => (
-            <FilterChoice key={ind} filterName={category} />
+            <FilterChoice
+              key={ind}
+              filterName={category}
+              updateQData={setQFormData}
+            />
           ))}
         </div>
         <h1 className="font-semibold text-3xl my-2 tracking-wider">
           DIFFICULTY
         </h1>
-        <img src="src/assets/waiting-page/Line.png" alt="line" />
-        <DifficultyButton />
+        <div className="flex flex-col items-center my-4 ">
+          <img
+            className="mb-2"
+            src="src/assets/waiting-page/Line.png"
+            alt="line"
+          />
+          <div className="flex justify-between w-11/12 -translate-y-6">
+            <DifficultyButton label="Easy" updateQData={setQFormData} />
+            <DifficultyButton label="Medium" updateQData={setQFormData} />
+            <DifficultyButton label="Hard" updateQData={setQFormData} />
+          </div>
+        </div>
       </form>
     </div>
   );
