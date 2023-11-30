5703a03c2d0cd62e0e7eae3b5277091a32a75e76
Ellie Xing <elliexin@usc.edu>
2023-11-29 21:10:05 -0800

    left panel UI changes

--- src/competition-page/FullCompPage.jsx
+++ src/competition-page/FullCompPage.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
-import { Panel, PanelGroup } from "react-resizable-panels";
 import { useNavigate } from "react-router-dom";
 import CompetitionPage from "./CompetitionPage";
-import ResizeHandle from "./ResizeHandle";
 import ProgressBar from "./ProgressBar";
 import Timer from "./Timer";
 
@@ -42,28 +40,23 @@ function FullCompPage() {
             <ProgressBar players={players} height={25} />
           </div>
           <div className="full-competition-page flex-grow flex">
-              <PanelGroup autoSaveId="full-competition-page" direction="horizontal">
-                <Panel defaultSizePercentage={35}>
-                  <div className="left-problem-desc" style={{padding: '20px' }}>
-                    <h2 className="problem-desc-title p-3 text-3xl font-bold tracking-wider">Two Sum</h2>
-                    <div className="selected-categories"></div>
-                    <p className="problem-desc p-3 font-medium">Given an array of integers <code>nums</code>&nbsp;and an integer <code>target</code>, return <em>indices of the two numbers such that they add up to <code>target</code></em>.
-                    <br/><br/>You may assume that each input would have <strong><em>exactly</em> one solution</strong>, and you may not use the <em>same</em> element twice.
-                    <br/><br/>You can return the answer in any order.
-                    <br/><br/><strong>Example:</strong>
-                    <br/><strong>Input:</strong> <code>nums = [2,7,11,15], target = 9</code>
-                    <br/><strong>Output:</strong> <code>[0,1]</code>
-                    <br/><strong>Explanation:</strong> <code>nums[0] + nums[1] == 9</code>
-                    </p>
-                  </div>
-                </Panel>
-                <ResizeHandle />
-                <Panel defaultSizePercentage={65}>
-                  <div className="right-IDE px-2">
-                    <CompetitionPage players={players} updatePlayer={updatePlayer}/>
-                  </div>
-                </Panel>
-              </PanelGroup>
+              <div className="left-problem-desc pl-5 w-[30%]">
+                <h2 className="problem-desc-title p-3 text-4xl font-bold tracking-wider relative">
+                  <span className="uppercase relative z-10">Two Sum</span>
+                  <span className="absolute inset-x-0 rounded bg-amber-50 bg-opacity-50 bottom-3 h-1/3"></span>
+                </h2>
+                <p className="problem-desc p-5 h-[80%] rounded bg-[#1b2b34] text-gray-300">Given an array of integers <code>nums</code>&nbsp;and an integer <code>target</code>, return <em>indices of the two numbers such that they add up to <code>target</code></em>.
+                <br/><br/>You may assume that each input would have <strong><em>exactly</em> one solution</strong>, and you may not use the <em>same</em> element twice.
+                <br/><br/>You can return the answer in any order.
+                <br/><br/><strong>Example:</strong>
+                <br/><strong>Input:</strong> <code>nums = [2,7,11,15], target = 9</code>
+                <br/><strong>Output:</strong> <code>[0,1]</code>
+                <br/><strong>Explanation:</strong> <code>nums[0] + nums[1] == 9</code>
+                </p>
+              </div>
+              <div className="right-IDE w-[70%] px-2">
+                <CompetitionPage players={players} updatePlayer={updatePlayer}/>
+              </div>
           </div>
         </div>
       </>
