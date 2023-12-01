54725c721f6118d1c174cad89b9c89e67799030e
VishnuVelayuthan <vishnuvelayuthan@gmail.com>
2023-12-01 08:14:14 -0800

    Got the game over page to work

--- src/game-over-page/GameOver.jsx
+++ src/game-over-page/GameOver.jsx
@@ -1,41 +1,51 @@
-import React from 'react';
+import React from "react";
 import "./GameOver.css";
 import { useNavigate } from "react-router-dom";
-import { useLocation } from 'react-router-dom';
+import { useWebSocket } from "../game-socket/WebSocketContext";
 
 const GameOver = () => {
-  const { state } = useLocation();
-  const players = state?.players || [];
-    const navigate = useNavigate();
+  const { gameState } = useWebSocket();
+  const navigate = useNavigate();
 
+  const players = Object.keys(gameState).map((username, ind) => ({
+    username: username,
+    testCasesPassed: gameState[username].testsPassed,
+    pointsGained: gameState[username].testsPassed * 10,
+  }));
+
+  players.sort((a, b) => b.pointsGained - a.pointsGained);
   return (
     <>
-        <div className="title-h2"><strong>Time's up!</strong></div>
-        {/* NOTE THAT BELOW IS COPIED FROM SIGN UP PAGE SO CLASS NAME IS NOT ACCURATE */}
-        <div className="sign-up-container">
-            <table>
-                <thead>
-                <tr>
-                    <th>Username</th>
-                    <th>Test Cases Passed</th>
-                    <th>Points Gained</th>
-                </tr>
-                </thead>
-                <tbody>
-                {players.map((player, index) => (
-                    <tr key={index}>
-                    <td>{player.username}</td>
-                    <td>{player.testCasesPassed}</td>
-                    <td>{player.pointsGained}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            <div className="sign-up-btn-container">
-                <button className="sign-up-button" onClick={() => navigate('/join')}>Leave game</button>
-            </div>
+      <div className="title-h2">
+        <strong>Time's up!</strong>
+      </div>
+      {/* NOTE THAT BELOW IS COPIED FROM SIGN UP PAGE SO CLASS NAME IS NOT ACCURATE */}
+      <div className="sign-up-container">
+        <table>
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Test Cases Passed</th>
+              <th>Points Gained</th>
+            </tr>
+          </thead>
+          <tbody>
+            {players.map((player, index) => (
+              <tr key={index}>
+                <td>{player.username}</td>
+                <td>{player.testCasesPassed}</td>
+                <td>{player.pointsGained}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="sign-up-btn-container">
+          <button className="sign-up-button" onClick={() => navigate("/join")}>
+            Leave game
+          </button>
         </div>
-      </>
+      </div>
+    </>
   );
 };
 
